55e4fb97c6158de3741898a999eee0fd55a1aaf6
adriankoder5 <adriankoder5@gmail.com>
2024-12-11 10:40:19 +0100

    D e  l A     y

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log('Hello :3');
+setTimeout(
+    ()=>{console.log('ferdig å laste')
+    },
+    10000
+);
+
+function delay(ms) {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, ms);
+    });
+};
+
+async function timeout () {
+    console.log('abcdefgijklmnopqrstuvwxyz')
+    await delay(5000);
+}
+
+timeout();
