d17f98774772cb303f13a56794005e5a7ade9ff3
adriankoder5 <adriankoder5@gmail.com>
2024-12-11 13:37:26 +0100

    ez

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 console.log('Hello :3');
 setTimeout(
-    ()=>{console.log('ferdig å laste')
+    ()=>{console.log('ferdig å laste');
     },
-    10000
+    2000
 );
+const parent = document.querySelector('main');
 
 function delay(ms) {
     return new Promise((resolve, reject) => {
@@ -12,8 +13,31 @@ function delay(ms) {
 };
 
 async function timeout () {
-    console.log('abcdefgijklmnopqrstuvwxyz')
+    console.log('abcdefgijklmnopqrstuvwxyz');
     await delay(5000);
 }
 
-timeout();
+//timeout();
+
+const myfacts = ['cat','quest']
+
+for (const i of myfacts) {
+    console.log(i);
+};
+
+
+
+async function getapidata () {
+    const response = await fetch('https://catfact.ninja/facts');
+    console.log(response);
+    const data = await response.json();
+    console.log('qwerty-----------------'+data.data[3].fact);
+    for (const i of data.data) {
+        console.log(i.fact);
+        const p = document.createElement('p');
+        p.textContent = i.fact;
+        parent.appendChild(p);
+    };
+};
+getapidata();
+
